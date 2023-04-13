4362c60f355be7f9e3958c978b8b111417954438
Andromeda <andromeda951@gmail.com>
2023-04-13 17:46:27 +0700

    Database Connection

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"belajar-gin-gorm/handler"
+	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	dsn := "root:@tcp(127.0.0.1:3306)/pustaka_api?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("DB connection error")
+	}
+
+	fmt.Println("Database connection succeed")
 
 	router := gin.Default()
 
